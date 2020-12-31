6e123ff983d7f8688a85d6f4727c115a35b96075
Syed Afrid Ali <aliatif908@gmail.com>
2020-12-31 17:25:48 +0530

    Shortcode added implementation

--- includes/class-wp-book.php
+++ includes/class-wp-book.php
@@ -183,6 +183,7 @@ class WP_Book {
 
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
+		$this->loader->add_action( 'init', $plugin_public, 'wp_book_register_shortcode' );
 
 	}
 
